31d8416e2edf91973325cb1857bbbad7a2f971d6
Abhishek Khandelwal <abhishekkhandelwal47@gmail.com>
2023-06-02 12:21:00 +0530

    signup and hashing the password

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 
@@ -12,6 +13,26 @@ const userSchema = new Schema({
         type: String,
         required: true,
     }
-}, { timestamps: true});
+});
+
+// static signup method
+// we can not use 'arrow function' here as we are using 'this' keyword inside the function
+// so this function needs to be defined as regular function
+userSchema.statics.signup = async function(email, password) {
+    const exists = await this.findOne({ email});
+
+    if(exists) {
+        throw Error('Email already in use');
+    }
+
+    // bcrypt adds a 'salt'(random) string to the password and hash it 
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const user = await this.create({ email, password: hash});
+
+    return user;
+
+}
 
 module.exports = mongoose.model('User', userSchema);
