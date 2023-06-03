d58b817f4ce3150081df5365a5614168c52845d6
Abhishek Khandelwal <abhishekkhandelwal47@gmail.com>
2023-06-03 15:09:16 +0530

    logout user using hooks

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -24,7 +24,7 @@ export const useSignup = () => {
         }
 
         if (response.ok) {
-            // save teh user to local storage
+            // save the user to local storage
             localStorage.setItem('user', JSON.stringify(json));
 
             // update the auth context
