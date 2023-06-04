ab6141792a0a5f1cd6245b1fd2fbb7c103815fab
Abhishek Khandelwal <abhishekkhandelwal47@gmail.com>
2023-06-04 17:34:30 +0530

    added cors policy

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
+// enable cross-origin middleware
+router.use(cors());
 // constroller functions
 const {signupUser, loginUser} = require('../controllers/userController');
 
@@ -10,5 +12,12 @@ router.post('/login', loginUser);
 
 // signup route
 router.post('/signup', signupUser);
+
+var app = express();
+app.use(cors());
+app.get('/test', function (req, res, next) {
+    res.json({msg: 'This is CORS-enabled for all origins!'})
+  })
+  
 
 module.exports = router;
